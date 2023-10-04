c0519071684aaa07ba2275368f477f8f102ff7c5
gyoenge <gyoengist@gm.gist.ac.kr>
2023-10-05 00:32:38 +0900

    fixed IconButton (reflected suggestion)

--- src/atoms/button_icon/IconButton.stories.tsx
+++ src/atoms/button_icon/IconButton.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryFn } from "@storybook/react";
-
 import IconButton from "./IconButton";
+import Icons from "src/assets/Icons";
+import colorSet from "src/styles/color-set";
 
 export default {
   title: "atoms/IconButton/IconButton",
@@ -14,6 +15,5 @@ export const Default = Template.bind({});
 Default.args = {
   text: "Button",
   fontSize: "20px",
-  width: "80px",
-  height: "30px",
+  icon: <Icons.Filter size="24px" fill={colorSet.titleText} />,
 };
--- src/atoms/button_icon/IconButton.tsx
+++ src/atoms/button_icon/IconButton.tsx
@@ -1,56 +1,26 @@
 import React from "react";
-import Icons from "src/assets/Icons";
+import Button from "../button/Button";
+import Flex from "../containers/flex/Flex";
+import Text from "../text/Text";
 import colorSet from "src/styles/color-set";
 import Font from "src/styles/fonts";
-import styled from "styled-components";
 
-import Text from "../text/Text";
-
-interface UniProps {
-  width?: React.CSSProperties["width"];
-  height?: React.CSSProperties["height"];
-}
-
-interface TextProps {
+interface IconButtonProps {
   text: string;
   fontSize?: React.CSSProperties["fontSize"];
+  icon: React.ReactNode;
 }
 
-interface ButtonProps extends UniProps, TextProps {}
-
-const Wrapper = styled.button<UniProps>`
-  min-width: ${({ width }) => (width ? width : undefined)};
-  width: fit-content;
-  height: ${({ height }) => (height ? height : undefined)};
-  background-color: transparent;
-  transition: 0.1s;
-  border-radius: 10px;
-  border: none;
-
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  gap: 5px;
-  padding: 20px 14px;
-
-  :hover {
-    cursor: pointer;
-    box-shadow: inset rgba(0, 0, 0, 0.15) 0px 0px 0px 40px;
-  }
-  :active {
-    transform: scale(0.95);
-  }
-`;
-
-const IconButton = ({ width, height, text, fontSize }: ButtonProps) => {
+const IconButton = ({ text, fontSize = "16px", icon }: IconButtonProps) => {
   return (
-    <Wrapper width={width} height={height}>
-      <Text color={colorSet.titleText} font={Font.Medium} size={fontSize}>
-        {text}
-      </Text>
-      <Icons.Filter size={`calc(${height}*0.8)`} fill={colorSet.titleText} />
-    </Wrapper>
+    <Button>
+      <Flex gap="5px" alignItems="center">
+        <Text color={colorSet.titleText} font={Font.Medium} size={fontSize}>
+          {text}
+        </Text>
+        {icon}
+      </Flex>
+    </Button>
   );
 };
 
