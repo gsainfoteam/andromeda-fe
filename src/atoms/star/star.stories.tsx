3a69935ecbc1d56abecb6e8fcd771003369bafea
leetaegyun <leetaegyun@gm.gist.ac.kr>
2023-11-11 15:17:16 +0900

    Star atom

---
+++ src/atoms/star/star.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryFn } from "@storybook/react";
+
+import Star from "./star";
+
+export default {
+  title: "atoms/star/Star",
+  component: Star,
+  argTypes: {
+    STAR_RATE: {
+      control: { type: 'range', min: 0, max: 5, step: 0.01}
+    }
+  }
+
+} as Meta<typeof Star>;
+
+const Template: StoryFn<typeof Star> = (args) => <Star {...args} />;
+
+export const Green = Template.bind({});
+Green.args = {
+  color: "green",
+  STAR_RATE: 3.7, //default STAR_RATE
+  width: 46, //default width & height (width==height)
+};
+
+export const Purple = Template.bind({});
+Purple.args = {
+  color: "purple",
+  STAR_RATE: 3.7, //default STAR_RATE
+  width: 46, //default width & height (width==height)
+};
+
