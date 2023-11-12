1e1e8b92c00a86adc05130c4057f3578f4b2ab2d
gyoenge <gyoengist@gm.gist.ac.kr>
2023-11-13 04:06:26 +0900

    fix: star 여러 번 사용 시 id 중복 수정

--- src/atoms/star/star.tsx
+++ src/atoms/star/star.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 import defaults from "src/styles/defaults";
 import Flex from "../containers/flex/Flex";
 
+import { v4 as uuidv4 } from "uuid"; // UUID 라이브러리
+
 export interface StarProps {
   color?: "green" | "purple";
   STAR_RATE: number;
@@ -11,6 +13,7 @@ export interface StarProps {
 }
 
 const Star = (props: StarProps) => {
+  const uniqueId = uuidv4();
   const DefaultColor = "#1A1A1A";
   const RATE = props.STAR_RATE;
   const STAR_IDX_ARR = ["first", "second", "third", "fourth", "last"]; // 5개 별의 고유 id
@@ -36,6 +39,7 @@ const Star = (props: StarProps) => {
       ? `${colorSet.galactic_green}`
       : `${colorSet.galactic_purple}`;
 
+  console.log(ratesResArr);
   return (
     <Flex
       alignItems="center"
@@ -43,28 +47,29 @@ const Star = (props: StarProps) => {
       style={{ borderRadius: defaults.borderRadius }}
     >
       {STAR_IDX_ARR.map((item, idx) => {
+        console.log(item);
         return (
           <span className="star_icon" key={`${item}_${idx}`}>
             <svg
-              xmlns="hhttp://www.w3.org/2000/svg"
+              xmlns="http://www.w3.org/2000/svg"
               width={props.starsize}
               height={props.starsize}
               viewBox="0 0 576 512"
               fill={DefaultColor}
               style={{ verticalAlign: "middle" }}
             >
-              <clipPath id={`${item}StarClip`}>
+              <clipPath id={`${item}StarClip-${uniqueId}`}>
                 <rect width={`${ratesResArr[idx]}`} height="512" />
               </clipPath>
               <path
-                id={`${item}Star`}
+                id={`${item}Star-${uniqueId}`}
                 stroke={DefColor}
                 stroke-width="35"
                 d="M381.2 150.3L524.9 171.5C536.8 173.2 546.8 181.6 550.6 193.1C554.4 204.7 551.3 217.3 542.7 225.9L438.5 328.1L463.1 474.7C465.1 486.7 460.2 498.9 450.2 506C440.3 513.1 427.2 514 416.5 508.3L288.1 439.8L159.8 508.3C149 514 135.9 513.1 126 506C116.1 498.9 111.1 486.7 113.2 474.7L137.8 328.1L33.58 225.9C24.97 217.3 21.91 204.7 25.69 193.1C29.46 181.6 39.43 173.2 51.42 171.5L195 150.3L259.4 17.97C264.7 6.954 275.9-.0391 288.1-.0391C300.4-.0391 311.6 6.954 316.9 17.97L381.2 150.3z"
               />
               <use
-                clipPath={`url(#${item}StarClip)`}
-                href={`#${item}Star`}
+                clipPath={`url(#${item}StarClip-${uniqueId})`}
+                href={`#${item}Star-${uniqueId}`}
                 fill={DefColor}
               />
             </svg>
